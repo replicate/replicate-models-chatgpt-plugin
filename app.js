7b57acb86d2c428fadd5855fcee02c957a859e49
Paul Hayes <fofr@users.noreply.github.com>
2023-04-14 11:53:23 +0100

    Run in parallel, abstract away version
    ChatGPT doesn't need to know about versions – we can save version numbers within the plugin app.
    
    This saves time as ChatGPT doesn't need to print out the long version number each time.
    
    We can also parallelise the running of each model. There's no need to do them sequentially.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import Replicate from 'replicate'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+const modelsAndVersions = {}
+
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 })
@@ -19,23 +21,63 @@ const __dirname = dirname(__filename)
 app.use(cors({ origin: 'https://chat.openai.com' }))
 app.use(express.json())
 
-app.post('/run', asyncHandler(async (req, res) => {
-  const { username, model, version, inputJSON } = req.body
+app.post('/run', asyncHandler(async (req, res, next) => {
+  const { models } = req.body
 
-  if (!username || !model || !version || !inputJSON) {
-    const missing = []
-    if (!username) missing.push('username')
-    if (!model) missing.push('model')
-    if (!version) missing.push('version')
-    if (!inputJSON) missing.push('inputJSON')
+  if (!models) {
+    res.status(400).json({ error: 'Missing required parameters: models' })
+    return
+  }
+
+  // if models is not an array
+  if (!Array.isArray(models)) {
+    res.status(400).json({ error: 'models must be an array' })
+    return
+  }
+
+  // if models is an empty array
+  if (models.length === 0) {
+    res.status(400).json({ error: 'models must be a non-empty array' })
+    return
+  }
+
+  // loop through models and check for required parameters
+  const missing = []
+  for (const m of models) {
+    const { username, model, input } = m
+    if (!username || !model || !input) {
+      const missingForModel = []
+      if (!username) missingForModel.push('username')
+      if (!model) missingForModel.push('model')
+      if (!input) missingForModel.push('input')
+      missing.push(`{ username: ${username}, model: ${model}, missing: ${missingForModel.join(',')} }`)
+    }
+  }
+
+  if (missing.length > 0) {
     res.status(400).json({ error: `Missing required parameters: ${missing.join(',')}` })
     return
   }
 
+  res.locals.models = models
+  next()
+}))
+
+app.post('/run', asyncHandler(async (req, res) => {
+  const { models } = req.body
+
   try {
-    const input = JSON.parse(inputJSON)
-    const output = await replicate.run(`${username}/${model}:${version}`, { input })
-    res.status(200).json(output)
+    const modelOutputs = await Promise.all(models.map(async ({ username, model, input }) => {
+      try {
+        const modelId = `${username}/${model}:${modelsAndVersions[username][model]}`
+        const output = await replicate.run(modelId, { input })
+        return { output, error: null }
+      } catch (e) {
+        return { output: null, error: e.message }
+      }
+    }))
+
+    res.status(200).json(modelOutputs)
   } catch (e) {
     res.status(500).json({ error: e.message })
   }
@@ -46,10 +88,12 @@ app.post('/model', asyncHandler(async (req, res) => {
   const response = await replicate.models.get(username, model)
   const output = {
     url: response.url,
-    version: response.latest_version.id,
     description: response.description,
     schema: response.latest_version.openapi_schema
   }
+
+  modelsAndVersions[username] = modelsAndVersions[username] || {}
+  modelsAndVersions[username][model] = response.latest_version.id
   res.status(200).json(output)
 }))
 
@@ -94,11 +138,11 @@ app.post('/collection', asyncHandler(async (req, res) => {
   const { collection_slug } = req.body
   const response = await replicate.collections.get(collection_slug)
   const models = response.models.map((model) => {
+    modelsAndVersions[model.owner][model.name] = model.latest_version.id
     return {
       url: model.url,
       username: model.owner,
       model: model.name,
-      version: model.latest_version.id,
       description: model.description
     }
   })
